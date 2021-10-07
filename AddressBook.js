ca5b7a52dd6ecf3b813cd9f3e1fdab26091fa769
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-07 22:44:45 +0530

    ADDED: created class contact, added fields and constructor

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,21 @@
-console.log("Welcome to Address Book System");
+class Contact{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zipCode;
+    phoneNumber;
+    email;
+    
+    constructor(... params){
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zipCode = params[5];
+    this.phoneNumber = params[6];
+    this.email = params[7];
+    }
+}
